9fc185f6187ce4525c74d0551ebd80bbb3d8182a
ram <ram@gmail.com>
2024-05-14 09:58:33 +0000

    Edited app.py

--- app.py
+++ app.py
@@ -1,10 +1,6 @@
-import unittest
 from selenium import webdriver
-from selenium.webdriver.common.by import By
-from selenium.webdriver.support.ui import WebDriverWait
-from selenium.webdriver.support import expected_conditions as EC
-from selenium.webdriver.edge.service import Service
-from webdriver_manager.microsoft import EdgeChromiumDriverManager
+import unittest
+from time import sleep
 
 class BookStore(unittest.TestCase):
     def setUp(self):
